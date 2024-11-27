0f6d3550622dff4e555b2d9bad6f3ef388199cfe
khalasit <tusharkhalasi115@gmail.com>
2024-11-27 03:26:19 -0800

    Create ComplexPlane.h

---
+++ code/ComplexPlane.h
@@ -0,0 +1,51 @@
+#pragma once
+#ifndef COMPLEXPLANE_H
+#define COMPLEXPLANE_H
+#include <SFML/Graphics.hpp>
+
+
+using namespace sf;
+
+const unsigned int MAX_ITER = 64;
+const float BASE_WIDTH = 4.0;
+const float BASE_HEIGHT = 4.0;
+const float BASE_ZOOM = 0.5;
+
+enum class State
+{
+	CALCULATING,
+	DISPLAYING
+};
+class ComplexPlane
+{
+	public:
+		ComplexPlane(int pixelWidth, int pixelHeight);
+
+		void draw(RenderTarget& target, RenderStates states) const;
+		void updateRender();
+		void zoomIn();
+		void zoomOut();
+		void setCenter(Vector2i mousePixel);
+		void setMouseLocation(Vector2i mousePixel);
+		void loadText(Text& text);
+
+		int countIterations(Vector2f coord);
+		void iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b);
+		Vector2f mapPixelToCoords(Vector2i mousePixel);
+
+	private:
+		VertexArray m_vArray;
+		State m_State;
+		Vector2f m_mouseLocation;
+		Vector2i m_pixel_size;
+		Vector2f m_plane_center;
+		Vector2f m_plane_size;
+		int m_zoomCount;
+		float m_aspectRatio;
+		
+};
+
+#endif COMPLEXPLANE_H
+
+
+
